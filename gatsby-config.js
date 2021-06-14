61abe93f20101e1fe0582021409c1b0468b36347
Daffa Arviano <daffa.arviano@outlook.com>
2021-06-14 15:02:27 +0700

    deployed project to firebase

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,18 +65,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#b75e09`,
         display: `standalone`,
-        icons: [
-          {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
+        icon: `static/favicon-64x64.gif`,
       },
     },
     `gatsby-plugin-offline`,
